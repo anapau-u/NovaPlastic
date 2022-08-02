bd590d6b82242c7bbc6c048769798030ff76ab1c
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-02 13:11:51 -0500

    menu master

--- menu-mas.php
+++ menu-mas.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>JAAPA's System</title>
+	<title>Master Menu</title>
 	<!--     Fonts and icons     -->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="images/icons/analista.png"/>
+	<link rel="icon" type="image/png" href="images/icons/master.png"/>
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
@@ -23,8 +23,8 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form" action="valida-form.php" method="post">
-					<span class="login100-form-title p-b-43">Menú Principal</span>
-					<br><br><br><br>
+					<span class="login100-form-title p-b-43">Menú Maestro</span>
+					<br>
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn"><a class="login100-form-btn" href="registrocliente-cap.php">Registro Cliente</a></button>
 					</div>
@@ -40,9 +40,46 @@
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn"><a class="login100-form-btn" href="tablaventas-cap.php">Ventas</a></button>
 					</div>
-				</form>
 
-				<div class="login100-more" style="background-image: url('');">
+                    <br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablaclientes-dir.php">Clientes</a></button>
+					</div>
+					<br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablaventas-dir.php">Ventas</a></button>
+					</div>
+                    <br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablareporte-dir.php">Reportes</a></button>
+					</div>
+                    <br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablausuarios-dir.php">Usuarios Registrados</a></button>
+					</div>
+                    <br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="registrousuario-dir.php">Registro Nuevo Usuario</a></button>
+					</div>
+
+                    <br>
+                    <div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="editarcliente-sup.php">Editar Cliente</a></button>
+					</div>
+					<br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="editarventa-sup.php">Editar Venta</a></button>
+					</div>
+					<br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablaclientes-sup.php">Clientes</a></button>
+					</div>
+					<br>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn"><a class="login100-form-btn" href="tablaventas-sup.php">Ventas</a></button>
+					</div>
+				</form>
+				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');">
 				</div>
 			</div>
 		</div>
