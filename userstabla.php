667699cb8324eb5e793e2d297c67e3b7ee4e6120
anapau-u <ana.villa.ch@usb.edu.mx>
2022-08-02 11:47:55 -0500

    update user

---
+++ userstabla.php
@@ -0,0 +1,43 @@
+<?php
+    while ($nreg=sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+    {
+        echo("<tr><td>".$nreg["iusuarios"]."</td>
+                    <td>".$nreg["usuario"]."</td>
+                    <td>".$nreg["nombre"]."</td>
+                    <td>".&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                    <td>&nbsp;%s&nbsp;</td>
+                </tr>",
+                $nreg["iusuarios"], 
+                $nreg["usuario"], 
+                $nreg["nombre"], 
+                $nreg["apaterno"], 
+                $nreg["amaterno"], 
+                $nreg["fnacimiento"], 
+                $nreg["puesto"], 
+                $nreg["telefono"], 
+                $nreg["pais"], 
+                $nreg["estado"], 
+                $nreg["municipio"], 
+                $nreg["colonia"],
+                $nreg["calle"],
+                $nreg["numeroint"],
+                $nreg["numeroext"],
+                $nreg["codpostal"],
+                $nreg["estatus"],
+                $nreg["falta"],
+                $nreg["iusuarios"]);
+    }
+?>
