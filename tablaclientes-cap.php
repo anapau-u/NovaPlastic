98a3c2eba2936e09b432d1003a6ad53d3391249a
anapau-u <ana.villa.ch@usb.edu.mx>
2022-08-02 16:41:04 -0500

    ip change

--- tablaclientes-cap.php
+++ tablaclientes-cap.php
@@ -44,7 +44,7 @@
           <tbody>
           <!-- CEO -->
           <?php
-            $serverName = "172.16.22.106, 1433";
+            $serverName = "192.168.100.5, 1433";
             $connectionInfo = array("Database"=>"JAAPA", "UID"=>"JAAPAPAM", "PWD"=>"123");
 
             $conn = sqlsrv_connect( $serverName, $connectionInfo );
