dea071c2d9cbcc2f2cc88adc4e0aedb34e3ca5df
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-04 15:15:10 -0500

    UWU

--- tablareporte-dir.php
+++ tablareporte-dir.php
@@ -16,6 +16,23 @@
 
     <title>Reporte - director</title>
   </head>
+  <?php
+    
+    $serverName = "172.16.22.106, 1433";
+    $connectionInfo = array("Database"=>"JAAPA", "UID"=>"JAAPAPAM", "PWD"=>"123");
+    $conn = sqlsrv_connect( $serverName, $connectionInfo );
+
+    if( $conn === false ) {
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+    $sql = "SELECT iventa, iempresa, importe, moneda, CAST(fecha as varchar) as fecha FROM Ventas";
+    $stmt = sqlsrv_query( $conn, $sql );
+    
+    if( $stmt === false) {
+        die( print_r( sqlsrv_errors(), true) );
+    }
+?>
   <body>
   <div class="content">
     <div class="container">
@@ -24,11 +41,16 @@
         <right><a class="login100-form-btn" href="menu-cap.php">Regresar al Menú</a></right>
       </div>
       <br>
-      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdown1" data-toggle="dropdown">Filtro</button>
-      <div class="dropdown-menu">
-      <a class="dropdown-item">Ejemplo</a>
-      <a class="dropdown-item">Ejemplo 2</a>
-  </div>
+      <div class="wrap-input100 validate-input" data-validate="Selecciona una Empresa">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100"></span>
+                    <select class="input100-select"  name="iventa" id="iventa"><br>
+                        <option value="0">Selecciona la Empresa</option>
+                        <?php while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {?>
+                            <option value="<?php echo $row['moneda']; ?>"><?php echo $row['fecha']; ?></option>
+                        <?php } sqlsrv_free_stmt( $stmt);?>
+                    </select>
+                    </div>
   <div class="table-responsive">
         <table class="table table-striped custom-table">
           <thead>
