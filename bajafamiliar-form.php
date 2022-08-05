53c0e246ce28a706696a2556239bc5039c09dc77
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-05 13:32:18 -0500

    cambios

--- bajafamiliar-form.php
+++ bajafamiliar-form.php
@@ -55,8 +55,7 @@
             <button class="login100-form-btn" action="menu-sup.php">Menu principal</button>
           </div>
         </form>
-
-				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');">
+        <div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');">
         </div>
       </div>
     </div>
