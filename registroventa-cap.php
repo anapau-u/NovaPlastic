0db6f41d7038c24378209d796163ce5bb3dc3dc3
anapau-u <ana.villa.ch@usb.edu.mx>
2022-08-05 20:12:28 -0500

    :)

--- registroventa-cap.php
+++ registroventa-cap.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Registro Ventas</title>
+	<title>Editar Cliente</title>
 	<!--     Fonts and icons     -->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="images/icons/editar.png"/>
+	<link rel="icon" type="image/png" href="images/icons/supervisor.png"/>
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
@@ -27,7 +27,10 @@
 		die( print_r( sqlsrv_errors(), true));
 	}
 
-	$sql = "SELECT usuario, puesto FROM usuarios";
+	$sql = "SELECT iventa, b.razonsocial 
+			FROM Ventas a
+			INNER JOIN Empresa b ON a.empresa=b.iempresa";
+
 	$stmt = sqlsrv_query( $conn, $sql );
 	
 	if( $stmt === false) {
@@ -47,62 +50,113 @@
 
     $sql = "SELECT iempresa, razonsocial FROM Empresa";
     $stmt = sqlsrv_query( $conn, $sql );
-    
-    if( $stmt === false) {
-        die( print_r( sqlsrv_errors(), true) );
-    }
+
 ?>
 <body style="background-color: #e9fff9;">
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="wrap-login100-left">
-				<form class="login100-form validate-form" action="registroventa-form.php" method="post">
-					<div class="container-login100-form-btn-right">
-						<left><a class="login100-form-btn-center" href="menu-cap.php">Volver al menú</a></left>
-					</div>
+			<div class="wrap-login100-center">
+				<form class="login100-form validate-form" action="editarcliente-form.php" method="post">
+					<span class="login100-form-title p-b-43">Editar información del cliente</span>
+					<center>Selecciona el registro que deseas editar<br> y llena únicamente los campos a modificar.</center>
 					<br>
-					<span class="login100-form-title p-b-43">Registro de Ventas</span>
-					
-					<div class="wrap-input100 validate-input" data-validate="Selecciona la Empresa">
-						<span class="focus-input100"></span>
-						<span class="label-input100"></span>
-						<select class="input100-select"  name="iempresa" id="iempresa"><br>
-							<option value="0">Selecciona la Empresa</option>
-							<?php while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {?>
-								<option value="<?php echo $row['iempresa']; ?>"><?php echo $row['razonsocial']; ?></option>
-							<?php } sqlsrv_free_stmt( $stmt);?>
-						</select>
+					<div class="wrap-input100" >
+                    <span class="focus-input100"></span>
+                    <span class="label-input100"></span>
+                    <select class="input100-select"  name="iempresa" id="iempresa"><br>
+                        <option value="0">Selecciona la Empresa</option>
+                        <?php while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {?>
+                            <option value="<?php echo $row['iempresa']; ?>"><?php echo $row['razonsocial']; ?></option>
+                        <?php } sqlsrv_free_stmt( $stmt);?>
+                    </select>
                     </div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Escribe el importe de la venta">
-						<input class="input100" type="number" name="importe">
+					
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="razonsocial">
 						<span class="focus-input100"></span>
-						<span class="label-input100">Importe</span>
+						<span class="label-input100">Razón Social</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Selecciona el tipo de cambio">
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="telefono">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Teléfono</span>
+					</div>
+
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="pais">
+						<span class="focus-input100"></span>
+						<span class="label-input100">País</span>
+					</div>
+
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="estado">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Estado</span>
+					</div>
+
+					<div class="wrap-input100" >
 						<span class="focus-input100"></span>
 						<span class="label-input100"></span>
-						<select class="input100-select" id="moneda" name="moneda">
-							<option value="vacio" selected>Selecciona el tipo de cambio</option>
-							<option value="Pesos">Pesos</option>
-							<option value="Dolares">Dólares</option>
+						<select class="input100-select" id="municipio" name="municipio">
+							<option value="vacio" selected>Selecciona tu Alcaldia</option>
+							<option value="AlvaroObregon">Álvaro Obregón</option>
+							<option value="Azcapotzalco">Azcapotzalco</option>
+							<option value="BenitoJuarez">Benito Juárez</option>
+							<option value="Coyoacan">Coyoacán</option>
+							<option value="Cuauhtemoc">Cuauhtémoc</option>
+							<option value="CuajimalpaDeMorelos">Cuajimalpa de Morelos</option>
+							<option value="GustavoAMadero">Gustavo A. Madero</option>
+							<option value="Iztacalco">Iztacalco</option>
+							<option value="Iztapalapa">Iztapalapa</option>
+							<option value="MagdalenaContrera">Magdalena Contreras</option>
+							<option value="MiguelHidalgo">Miguel Hidalgo</option>
+							<option value="MilpaAlta">Milpa Alta</option>
+							<option value="Tlahuac">Tláhuac</option>
+							<option value="Tlalpan">Tlalpan</option>
+							<option value="VenustianoCarranza">Venustiano Carranza</option>
+							<option value="Xochimilco">Xochimilco</option>
 						</select>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Selecciona una fecha">
-						<input class="input100" type="date" name="fecha">
-						<!-- <span class="focus-input100">Fecha</span>
-						<span class="label-input100">Fecha</span> -->
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="colonia">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Colonia</span>
 					</div>
 
-					<br><br>
+					<div class="wrap-input100" >
+						<input class="input100" type="text" name="calle">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Calle</span>
+					</div>
+          
+					<div class="wrap-input100" >
+						<input class="input100" type="number" name="numeroint">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Número interior</span>
+					</div>
+
+					<div class="wrap-input100" >
+						<input class="input100" type="number" name="numeroext">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Número exterior</span>
+					</div>
+          
+					<div class="wrap-input100" >
+						<input class="input100" type="number" name="codpostal">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Código Postal</span>
+					</div>
+
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">Registrar</button>
+					<input class="login100-form-btn" type="submit" value="Actualizar">
 					</div>
 					<br>
 				</form>
-				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1599027619483-9a92c1542d6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80');">
+				<!-- <img src="https://images.unsplash.com/photo-1525498128493-380d1990a112?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"> -->
+
+				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1521459382675-a3f2f35a6b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80');">
 				</div>
 			</div>
 		</div>
