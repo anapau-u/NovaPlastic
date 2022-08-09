19bf5bf0aee8ec43e1b2433a18a5a088b06c8462
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-09 11:42:18 -0500

    correo

--- pruebas.php
+++ pruebas.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pruebas</title>
-</head>
-<body>
-    <?php
-    $serverName = "192.168.100.52, 1433";
-    $connectionInfo = array("Database"=>"JAAPA", "UID"=>"JAAPAPAM", "PWD"=>"123");
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $conn = sqlsrv_connect( $serverName, $connectionInfo );
-    if( $conn === false ) {
-        die( print_r( sqlsrv_errors(), true));
-    }
-
-    $varusu=$_POST["usuario"];
-    $varpwd=$_POST["contra"];
-
-    $sql="exec sp_valida ".$varusu.", ".$varpwd;
-    $stmt=sqlsrv_query( $conn, $sql);
-
-    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-        echo $row['mensaje']."<br />";
-    }
-
-    ?>
-</body>
-</html>
+require 'correo/Exception.php';
+require 'correo/PHPMailer.php';
+require 'correo/SMTP.php';
+?>
