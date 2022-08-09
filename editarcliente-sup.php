dbb12cb0b087964f01025b0e229a788509a529d9
anapau-u <ana.villa.ch@usb.edu.mx>
2022-08-09 14:26:01 -0500

    cwnm

--- editarcliente-sup.php
+++ editarcliente-sup.php
@@ -69,16 +69,7 @@
 					<span class="login100-form-title p-b-43">Editar información del cliente</span>
 					<center>Selecciona el registro que deseas editar<br> y llena únicamente los campos a modificar.</center>
 					<br>
-					<div class="wrap-input100" >
-                    <span class="focus-input100"></span>
-                    <span class="label-input100"></span>
-                    <select class="input100-select"  name="iempresa" id="iempresa"><br>
-                        <option value="0">Selecciona la Empresa</option>
-                        <?php while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {?>
-                            <option value="<?php echo $row['iempresa']; ?>"><?php echo $row['razonsocial']; ?></option>
-                        <?php } sqlsrv_free_stmt( $stmt);?>
-                    </select>
-                    </div>
+					
 					<?php
 						while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 
