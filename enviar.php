6400c2495e3f2cc5e9e09638fd18ea6830dc6dc3
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-11 12:04:19 -0500

    correo

---
+++ enviar.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Mailer/src/Exception.php';
+require 'Mailer/src/PHPMailer.php';
+require 'Mailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+//$mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
+
+try {
+	$mail->SMTPOptions = array(
+		'ssl'=> array(
+		'verify_peer'=> false,
+		'verify_depth'=> false,
+		'verify_peer_name'=> false,
+		'allow_self_signed'=> true
+		)
+	);
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    //$mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.office365.com';                    //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jaapatech.2022@outlook.com';                     //SMTP username
+    $mail->Password   = 'jaapasa1234';                               //SMTP password
+    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->SMTPSecure = 'STARTTLS';
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set
+
+    //Recipients
+    $mail->setFrom('jaapatech.2022@outlook.com', 'JAAPA');
+    $mail->addAddress('pamela.chavez.oj@usb.edu.mx', 'Pamela');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Prueba3';
+    $mail->Body    = 'YAAAAAAAAAAAAAAAAAAA QUEDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO';
+    $mail->AltBody = 'uwu';
+
+    $mail->send();
+    echo 'Mensaje enviado';
+
+} catch (Exception $e) {
+    echo "Error: {$mail->ErrorInfo}";
+}
