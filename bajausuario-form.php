2fc52d65c2452d0c339039b3935f70fc9e1e15fb
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-11 14:42:54 -0500

    imagen

--- bajausuario-form.php
+++ bajausuario-form.php
@@ -85,7 +85,7 @@
           </div>
         </form>
 
-				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');">
+				<div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80');">
         </div>
       </div>
     </div>
