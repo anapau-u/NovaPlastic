6ec8191368256b2e5296049f27895e3ae430296c
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-12 11:12:08 -0500

    cambios varios

--- tablareporte-dir.php
+++ tablareporte-dir.php
@@ -18,7 +18,7 @@
   </head>
   <?php
     
-    $serverName = "192.168.100.52, 1433";
+    $serverName = "172.16.22.106, 1433";
     $connectionInfo = array("Database"=>"JAAPA", "UID"=>"JAAPAPAM", "PWD"=>"123");
     $conn = sqlsrv_connect( $serverName, $connectionInfo );
 
