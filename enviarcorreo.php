4b407025ba7a9041d8538568238c6448d1f85adf
Jackie Charles <110050161+jackie-charms@users.noreply.github.com>
2022-08-12 12:39:15 -0500

    cambios

--- enviarcorreo.php
+++ enviarcorreo.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Inicio de sesión</title>
+  <!--     Fonts and icons     -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="images/icons/casa.png"/>
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+  <!--     CSS Files     -->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css"> 
+  <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body style="background-color: #e9fff9;">
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        <form class="login100-form validate-form" action="menu-cap.php" method="post">
+          <input type="hidden" name="nick" value="<?php echo $usuario; ?>">
+          <input type="hidden" name="pass" value="<?php echo $contra; ?>">
+          <span class="login100-form-title p-b-43">
+            Correo enviado!
+          </span>
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -35,7 +65,7 @@ try {
 
     //Recipients
     $mail->setFrom('jaapatech.2022@outlook.com', 'NovaPlastic');
-    $mail->addAddress('jacqueline.charles.se@usb.edu.mx', 'Jackie');
+    $mail->addAddress('');
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
@@ -48,3 +78,5 @@ try {
 } catch (Exception $e) {
     echo "Error: {$mail->ErrorInfo}";
 }
+?>
+    <br>
